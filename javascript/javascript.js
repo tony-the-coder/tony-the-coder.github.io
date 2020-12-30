eba4a189d4e8a8b3bfdf50c98fe814d9427cfc5d
Tony Coder <59034553+tony-the-coder@users.noreply.github.com>
2020-12-29 20:18:39 -0500

    Completed project, but unable to remove lightbox2-dev because of VS errors.

---
+++ javascript/javascript.js
@@ -0,0 +1,41 @@
+// onload(alert("I am testing JavaScript link just to be safe!"));
+
+
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+
+window.onscroll = function () {
+    if (window.pageYOffset > 100) {
+        navbar.classList.remove('top');
+        if (!scrolled) {
+            navbar.style.transform = 'translateY(-70px)';
+        }
+        setTimeout(function () {
+            navbar.style.transform = 'translateY(0)';
+            scrolled = true;
+        }, 500)
+    } else {
+        navbar.classList.add('top');
+        scrolled = false;
+    }
+}
+
+
+//For smooth scrolling
+$('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100,
+            },
+            800
+        );
+
+    }
+
+});
